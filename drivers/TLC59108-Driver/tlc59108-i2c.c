ed65015c0007461df9148a210e1011121b73726a
David Weber <david@david-weber.com>
2011-09-18 17:13:11 -0500

    added file operations to driver

--- drivers/TLC59108-Driver/tlc59108-i2c.c
+++ drivers/TLC59108-Driver/tlc59108-i2c.c
@@ -45,7 +45,10 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/interrupt.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 #include <linux/i2c.h>
+#include "omap3_beagle_ioctl.h"
 
 /*
  *  TLC59108 device registers
@@ -80,13 +83,143 @@
 
 #define AI_MODE				TLC59108_AI_NONE
 
-#define DEBUG
+//#define DEBUG
 
 #ifdef DEBUG
-#define DEBUG_TLC59108(fmt...) printk(fmt)
+#define DPRINTK(fmt...) printk(fmt)
 #else
-#define DEBUG_TLC59108(fmt...) do { } while(0)
+#define DPRINTK(fmt...)
 #endif
+
+#define DRIVER_DESC "TLC59108 I2C driver"
+#define DRIVER_NAME "tlc59108-i2c"
+
+typedef struct inode inode_t;
+typedef struct file file_t;
+typedef struct semaphore semaphore_t;
+typedef struct cdev cdev_t;
+typedef struct file_operations driver_funcs_t;
+
+typedef struct
+{
+    uint8_t  num_readers;
+//    wait_queue_head_t wait_q;
+    cdev_t cdev;
+    dev_t dev;
+    // add other members as needed for a given driver
+    struct tlc59108_i2c *tlc59108_i2c_dev;;
+} dev_config_t;
+
+static int device_open (inode_t *inodep, file_t *filp);
+static long device_ioctl (file_t *filp, unsigned int cmd, unsigned long arg);
+static int device_release (inode_t *inodep, file_t *filp);
+static int my_device_create(dev_config_t* devptr, driver_funcs_t* funcs);
+static inline int tlc59108_i2c_write_reg(struct tlc59108_i2c *dev, u8 reg_addr, u8 data);
+static inline int tlc59108_i2c_read_reg(struct tlc59108_i2c *dev, u8 reg_addr, u8* data);
+
+static driver_funcs_t driver_funcs =
+{
+  .owner = THIS_MODULE,
+  .unlocked_ioctl = device_ioctl,
+  .open = device_open,
+  .release = device_release
+};
+
+static dev_config_t dev_config;
+
+static long device_ioctl (file_t *filp, unsigned int arg1, unsigned long arg2)
+{
+    u8 data = 0;
+    u8 addr = 0;
+
+    switch(arg1)
+    {
+      case OMAP3_BEAGLE_LCD_IOCT_ON:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_LCD_IOCT_ON", arg2);
+        tlc59108_i2c_write_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT0, 0x00);
+        break;
+
+      case OMAP3_BEAGLE_LCD_IOCT_OFF:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_LCD_IOCT_OFF", arg2);
+        tlc59108_i2c_write_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT0, 0x10);
+        break;
+
+      case OMAP3_BEAGLE_LCD_IOCT_TOGGLE:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_LCD_IOCT_TOGGLE", arg2);
+        tlc59108_i2c_read_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT0, &data);
+        DPRINTK(DRIVER_DESC " data = 0x%02X\n", data);
+        tlc59108_i2c_write_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT0, (data ^ 0x10));
+        break;
+
+      case OMAP3_BEAGLE_LCD_IOCT_FLIP:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_LCD_IOCT_TOGGLE", arg2);
+        tlc59108_i2c_read_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT1, &data);
+        DPRINTK(DRIVER_DESC " data = 0x%02X\n", data);
+        tlc59108_i2c_write_reg(dev_config.tlc59108_i2c_dev, TLC59108_REG_LEDOUT1, (data ^ 0x01));
+        break;
+
+      case OMAP3_BEAGLE_I2C_IOCQ_RD:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_I2C_IOCQ_RD", arg2);
+        addr = arg2;
+        tlc59108_i2c_read_reg(dev_config.tlc59108_i2c_dev, addr, &data);
+        DPRINTK(DRIVER_DESC " addr = 0x%02X, data = 0x%02X\n", addr, data);
+        break;
+
+      case OMAP3_BEAGLE_I2C_IOCT_WR:
+        DPRINTK(DRIVER_DESC " %s(%s, 0x%08X)\n", __FUNCTION__, "OMAP_BEAGLE_I2C_IOCT_WR", arg2);
+        data = (arg2 >> 8);
+        addr = arg2;        
+        DPRINTK(DRIVER_DESC " addr = 0x%02X, data = 0x%02X\n", addr, data);
+        tlc59108_i2c_write_reg(dev_config.tlc59108_i2c_dev, addr, data);
+        break;
+
+      default:
+        break;
+    }
+    return data;
+}
+
+static int device_open (inode_t *inodep, file_t *filp)
+{
+    filp->private_data = &dev_config;
+
+    DPRINTK(KERN_INFO DRIVER_DESC " %s()\n", __FUNCTION__);
+    return 0;
+}
+
+static int device_release (inode_t *inodep, file_t *filp)
+{
+    DPRINTK(KERN_INFO DRIVER_DESC " %s()\n", __FUNCTION__);
+    return 0;
+}
+
+static int my_device_create(dev_config_t* dev_configp, driver_funcs_t* funcs)
+{
+    int ret = 0;
+
+    DPRINTK(KERN_INFO "Initializing " DRIVER_DESC "\n");
+
+    // initialize device configuration structure used by this driver
+    dev_configp->num_readers = 0;
+
+//    init_waitqueue_head(&dev_configp->wait_q);
+
+    // dynamically allocate driver major/minor numbers - the device file 
+    // must still be created (presumably by an init script)
+    ret = alloc_chrdev_region(&dev_configp->dev,  0, 1, DRIVER_NAME);
+
+    if (ret == 0)
+    {
+        DPRINTK(KERN_INFO "Registering " DRIVER_DESC "\n");
+
+        // register the device driver - associate driver funcs w/ device
+        cdev_init(&dev_configp->cdev, funcs);
+        dev_configp->cdev.ops = funcs;
+        dev_configp->cdev.owner = THIS_MODULE;
+        ret = cdev_add(&dev_configp->cdev, dev_configp->dev, 1);
+    }
+    return ret;
+}
 
 struct tlc59108_i2c {
         char			name[I2C_NAME_SIZE];	
@@ -123,6 +256,7 @@ static inline int tlc59108_i2c_read_reg(struct tlc59108_i2c *dev, u8 reg_addr, u
 
 	dev_dbg(&dev->client->dev, "ret: 0x%x, data: 0x%x\n", ret, *data);
 	
+        DPRINTK(DRIVER_DESC " %s: addr = 0x%x, data = 0x%x\n", __FUNCTION__, reg_addr, *data);
 	return ret;
 }
 
@@ -248,12 +382,12 @@ static const struct attribute_group tlc59108_i2c_attr_group = {
  */
 
 static int __devinit tlc59108_i2c_probe(struct i2c_client *new_client,
-				   const struct i2c_device_id *id)
+	  			   const struct i2c_device_id *id)
 {
 	struct tlc59108_i2c *dev;
 	int err;
          
-        DEBUG_TLC59108("%s: entered\n", __FUNCTION__);
+        DPRINTK("%s: entered\n", __FUNCTION__);
 
 	if (!i2c_check_functionality(new_client->adapter,
 				     I2C_FUNC_I2C))
@@ -268,6 +402,9 @@ static int __devinit tlc59108_i2c_probe(struct i2c_client *new_client,
 
 	// Assign structure members
 	dev->client = new_client;
+
+        // keep a handle for use by file ops
+        dev_config.tlc59108_i2c_dev = dev;
 
 	// Initialize certain struct data
 	dev->regaddr = 0;
@@ -292,10 +429,12 @@ static int __devexit tlc59108_i2c_remove(struct i2c_client *client)
 {
         struct tlc59108_i2c *dev;
 
-      	DEBUG_TLC59108("%s: entered\n",__FUNCTION__);
+      	DPRINTK("%s: entered\n",__FUNCTION__);
 	dev_dbg(&client->dev,"%s: dev_dbg message\n",__FUNCTION__);
 
 	dev = i2c_get_clientdata(client);
+
+        sysfs_remove_group(&client->dev.kobj, &tlc59108_i2c_attr_group);
 
 	kfree(dev);
 
@@ -321,9 +460,26 @@ static struct i2c_driver tlc59108_i2c_driver = {
 
 static int __init tlc59108_i2c_init(void)
 {
-        DEBUG_TLC59108("%s: entered\n", __FUNCTION__);
+    int ret = 0;
 
-	return i2c_add_driver(&tlc59108_i2c_driver);
+    DPRINTK("%s: entered\n", __FUNCTION__);
+
+    ret = my_device_create(&dev_config, &driver_funcs);
+
+    if (ret == 0)
+    {
+        // initialize hardware
+        DPRINTK(KERN_INFO "SUCCESS - major:%d, minor:%d\n",
+               MAJOR(dev_config.dev), MINOR(dev_config.dev));
+
+        ret = i2c_add_driver(&tlc59108_i2c_driver);
+    }
+    else
+    {
+        DPRINTK(KERN_INFO "FAILURE\n");
+    }
+    DPRINTK("%s: exiting, ret = %d\n", __FUNCTION__, ret);
+    return ret;
 }
 
 static void __exit tlc59108_i2c_exit(void)
