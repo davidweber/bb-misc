c73e6e22a515df1f70c237c9a862d75e3c6214d0
David Weber <david@david-weber.com>
2011-10-05 20:09:25 -0700

    removed file operations and added dev_create()

--- drivers/char_module/char_module_2.c
+++ drivers/char_module/char_module_2.c
@@ -2,31 +2,51 @@
 #include <linux/kernel.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
+#include "cdev_types.h"
 
-#define MODULE_NAME "char_module"
+#define MODULE_NAME "char_module_2"
 
-// simplifying typedefs - move to .h file for easy re-use
-typedef struct inode inode_t;
-typedef struct file file_t;
+// define a structure to hold driver info
+typedef struct
+{
+  cdev_t cdev;
+  dev_t dev;
+  // add other members as needed for a given driver
+} dev_config_t;
 
 // driver function prototypes
-static int     dev_open   (inode_t* inodep, file_t* fp);
-static int     dev_close  (inode_t* inodep, file_t* fp);
+static int     dev_create (dev_config_t* dev_conf_p, driver_funcs_t* funcs_p);
+
+static driver_funcs_t driver_funcs =
+{
+  .owner          = THIS_MODULE,
+};
+
+static dev_config_t dev_config;
 
 //-----------------------------------------------------------------------------
 
-static int dev_open (inode_t* inodep, file_t* fp)
+static int dev_create(dev_config_t* dev_config_p, driver_funcs_t* funcs_p)
 {
-  printk("%s - %s\n", MODULE_NAME, __FUNCTION__);
-  return 0;
-}
+    int ret = 0;
 
-//-----------------------------------------------------------------------------
+    printk("%s - %s: initializing module...\n", MODULE_NAME, __FUNCTION__);
 
-static int dev_close (inode_t* inodep, file_t* fp)
-{
-  printk("%s - %s\n", MODULE_NAME, __FUNCTION__);
-  return 0;
+    // dynamically allocate driver major/minor numbers - the device file 
+    // must still be created (presumably by an init script)
+    ret = alloc_chrdev_region(&dev_config_p->dev,  0, 1, MODULE_NAME);
+
+    if (ret == 0)
+    {
+        printk("%s - %s: registering module...\n", MODULE_NAME, __FUNCTION__);
+
+        // register the device driver - associate driver funcs w/device
+        cdev_init(&dev_config_p->cdev, funcs_p);
+        dev_config_p->cdev.ops = funcs_p;
+        dev_config_p->cdev.owner = THIS_MODULE;
+        ret = cdev_add(&dev_config_p->cdev, dev_config_p->dev, 1);
+    }
+    return ret;
 }
 
 //-----------------------------------------------------------------------------
@@ -37,6 +57,8 @@ int init_module(void)
 
   printk("%s - %s\n", MODULE_NAME, __FUNCTION__);
 
+  status = dev_create(&dev_config, &driver_funcs);
+
   return status;
 }
 
@@ -44,12 +66,12 @@ int init_module(void)
 
 void cleanup_module(void)
 {
-  printk("%s - %s\n", MODULE_NAME, __FUNCTION__);
+  printk("%s - %s: unregistering module...\n", MODULE_NAME, __FUNCTION__);
+  unregister_chrdev_region(dev_config.dev, 1);
   return;
 }
 
 //-----------------------------------------------------------------------------
-
 
 MODULE_ALIAS("test:"MODULE_NAME);
 MODULE_AUTHOR("Linux Student");
