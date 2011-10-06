dc8bbe8565b23de532e0d3600fd6e3653746b84f
David Weber <david@david-weber.com>
2011-10-05 20:06:57 -0700

    add file with common character driver typedefs

---
+++ drivers/char_module/cdev_types.h
@@ -0,0 +1,14 @@
+/**
+ * @file driver_types.h
+ */
+
+#ifndef __DRIVER_TYPES_H__
+#define __DRIVER_TYPES_H__
+
+// simplifying driver typedefs
+typedef struct inode inode_t;
+typedef struct file file_t;
+typedef struct cdev cdev_t;
+typedef struct file_operations driver_funcs_t;
+
+#endif //__DRIVER_TYPES_H__
